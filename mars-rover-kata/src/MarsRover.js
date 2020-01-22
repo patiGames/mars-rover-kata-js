2f3b9eaf5e2fdc206c3bdfbaaa04cac95e321698
enricjorda <enric.jorda@codurance.com>
2020-01-22 11:58:16 +0100

    Add turn to rover

--- mars-rover-kata/src/MarsRover.js
+++ mars-rover-kata/src/MarsRover.js
@@ -1,4 +1,4 @@
-export default class MarsRover {
+export default class Rover {
   constructor(x, y, direction) {
     this.x = x;
     this.y = y;
@@ -7,5 +7,20 @@ export default class MarsRover {
 
   get position() {
     return `${this.x} ${this.y} ${this.direction}`;
+  }
+
+  implement(commands) {
+    for (let command of commands) {
+      if (command === 'r') this.turnRight();
+      else if (command === 'b') this.y--;
+      else this.y++;
+    }
+    return this.position;
+  }
+
+  turnRight() {
+    if (this.direction === 'S') this.direction = 'W';
+    else if (this.direction === 'E') this.direction = 'S';
+    else this.direction = 'E';
   }
 }
--- mars-rover-kata/test/MarsRoverShould.js
+++ mars-rover-kata/test/MarsRoverShould.js
@@ -1,9 +1,49 @@
 import { expect } from 'chai';
-import MarsRover from '../src/MarsRover';
+import Rover from '../src/MarsRover';
 
 describe('MarsRoverShould', () => {
+  let rover;
+  beforeEach(() => {
+    rover = new Rover(5, 5, 'N');
+  });
+
   it('have position of 5 5 N on initialization', () => {
-    let marsRover = new MarsRover(5, 5, 'N');
-    expect(marsRover.position).to.equal('5 5 N');
+    expect(rover.position).to.equal('5 5 N');
+  });
+
+  let tests = [
+    { args: 'f', expected: '5 6 N' },
+    { args: 'ff', expected: '5 7 N' },
+    { args: 'fff', expected: '5 8 N' }
+  ];
+
+  tests.forEach(test => {
+    it(`move forward ${test.args}`, () => {
+      expect(rover.implement(test.args)).to.equal(test.expected);
+    });
+  });
+
+  tests = [
+    { args: 'b', expected: '5 4 N' },
+    { args: 'bb', expected: '5 3 N' },
+    { args: 'bbb', expected: '5 2 N' }
+  ];
+
+  tests.forEach(test => {
+    it(`move backward ${test.args}`, () => {
+      expect(rover.implement(test.args)).to.equal(test.expected);
+    });
+  });
+
+  tests = [
+    { args: 'r', expected: '5 5 E' },
+    { args: 'rr', expected: '5 5 S' },
+    { args: 'rrr', expected: '5 5 W' }
+  ];
+
+  tests.forEach(test => {
+    it(`turn right ${test.args}`, () => {
+      expect(rover.implement(test.args)).to.equal(test.expected);
+    });
   });
 });
